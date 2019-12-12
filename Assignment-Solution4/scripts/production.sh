0667eb873957490b0ddbf1571a0ff3e3da7b854c
C <9006411+cdenoo@users.noreply.github.com>
2019-12-12 12:26:39 +0100

    removed unnecessary wait

--- Assignment-Solution4/scripts/production.sh
+++ Assignment-Solution4/scripts/production.sh
@@ -1,5 +1,4 @@
 #! /usr/bin/bash
 
 vagrant ssh -c "docker-compose -f /vagrant/files/docker-compose-setup.yml down"
-sleep 10
 vagrant ssh -c "docker-compose -f /vagrant/files/docker-compose.yml up -d"
